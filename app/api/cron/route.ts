ca5f75db4ab718aca8e5a5cc1e2958809304b3e3
MrAHMED <chikhaoui084@gmail.com>
2024-07-24 19:44:30 -0700

    Update cron job

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -2,12 +2,12 @@ import { getAllProducts2 } from "@/lib/action"
 import type { NextRequest } from "next/server"
 
 export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization")
+  /*const authHeader = request.headers.get("authorization")
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response("Unauthorized", {
       status: 401,
     })
-  }
+  }*/
   const result = await getAllProducts2({})
-  return Response.json({ success: true, result })
+  return Response.json({ success: true })
 }
